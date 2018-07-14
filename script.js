6cd5ce3b3ad62188d2c1fb870dd594a593ea8ab6
Sam Spencer <sspencerbtv@gmail.com>
2018-07-14 18:51:51 -0400

    added styling, color, heights, widths, general layout

--- script.js
+++
@@ -1,8 +0,0 @@
-function toPreview() {
-    let input = document.getElementById("source").value
-    let preview = document.getElementById("preview")
-    
-    preview.innerHTML = marked(input)
-
-    
-}
