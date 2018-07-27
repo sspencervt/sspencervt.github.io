e93214ecca23c7e9cb658e27e01054957c1f1c96
Sam Spencer <sspencerbtv@gmail.com>
2018-07-27 14:35:11 -0400

    at a finished state, jquery scrolling disabled.

--- portfolio.js
+++ portfolio.js
@@ -1,20 +1,29 @@
 
+// $(document).ready(function() {
+//     var scrollLink = $('.menubuttons');
+//     console.log(scrollLink)
+//     scrollLink.click(function(e) {
+//         e.preventDefault();
+//         $('#fullpage').animate({
+//             scrollTop: $(this.hash).offset().top
+//         }, 1000 );
+//     })
+// })
 
-$(document).ready(function() {
-  
-    var scrollLink = $('.menubuttons');
-    
-    // Smooth scrolling
-    scrollLink.click(function(e) {
-      e.preventDefault();
-      let selector = $('body,html')
-      console.log({selector});
-      console.log({e});
-      console.log(this.hash);
-      let target = $(this.hash);
-      console.log({target});
-      selector.animate({
-        scrollTop: target.offset().top
-      }, 1000 );
-    });
-});
+// $(document).ready(function() {
+//     $("a").on('click', function(e) {
+//         if (this.hash !== "") {
+//             e.preventDefault();
+//             let hash = this.hash;
+//             console.log(hash)
+//             $('#fullpage').animate({
+                
+//                 scrollTop: $(hash).offset().top
+                
+//             }, 1000, function() {
+//                 window.location.hash = hash;
+//             })
+//         }
+//     })
+
+// })
